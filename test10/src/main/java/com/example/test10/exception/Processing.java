0094a71769bdcd6d24623e9191df19c55ed6b981
sioloni <gorshkovni20@st.ithub.ru>
2023-04-15 03:11:02 -0400

    mext

--- test10/src/main/java/com/example/test10/exception/Processing.java
+++ test10/src/main/java/com/example/test10/exception/Processing.java
@@ -4,16 +4,9 @@ public enum Processing {
     SEARCH_MODEL_BY_ID{
         @Override
         public Exception getException() {
-            return new RuntimeException("id is not found");
+            return new NoSuchFieldException("id is not found");
         }
     };
-
-//    private Exception exception;
-//
-//
-//    Processing(Exception exception) {
-//        this.exception = exception;
-//    }
 
 
     public abstract Exception getException();
